4b2125bd8d208cc9ef987eb1cc34fefe374b9a52
Hridita-11 <hridita.nz@gmail.com>
2026-02-03 19:22:01 +1300

    Add files via upload

---
+++ avarage.py
@@ -0,0 +1,9 @@
+tree1 = 23
+tree2 = 51
+tree3 = 44
+tree4 = 9
+tree5 = 98
+sum= tree1+tree2+tree3+tree4+tree5
+print("the sum of all the tree is:",sum)
+average= sum/5
+print ("the average of all the tre is:",average)
